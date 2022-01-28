e910ce491a792e61063f250cbbd2ba8d19183163
BrandonLWhite <brandonlwhite@gmail.com>
2022-01-28 13:03:50 -0600

    Added namespace to function name to avoid conflicting with other plugins

--- etc/pyenv.d/install/alias-latest-patch-version.bash
+++ etc/pyenv.d/install/alias-latest-patch-version.bash
@@ -1,16 +1,16 @@
 # See https://github.com/pyenv/pyenv/wiki/Authoring-plugins
 
-# before_install_func() {
+# alias_latest_patch_version__before_install_func() {
 # }
 
 # Detect if the installation command made use of the `M.m:latest` format, and if so, create a symlink that points
 # `M.m` to the `M.m.p` directory that was installed.
-after_install_func() {
+alias_latest_patch_version__after_install_func() {
     if [ "${DEFINITION}" != "${DEFINITION_PREFIX}" ]; then
         echo "Setting alias '${DEFINITION_PREFIX}' for '${PREFIX}'"
         ln -sf "${DEFINITION}" "${PREFIX}/../${DEFINITION_PREFIX}"
     fi
 }
 
-# before_install "before_install_func"
-after_install "after_install_func"
+# before_install "alias_latest_patch_version__before_install_func"
+after_install "alias_latest_patch_version__after_install_func"
