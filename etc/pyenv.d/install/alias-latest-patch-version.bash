35e7eb4d66401b662dbd5aa191f4fd41459331cd
BrandonLWhite <brandonlwhite@gmail.com>
2022-04-15 17:16:49 -0500

    Replace `ln -sfT` with `rm -f` and `ln -s` for MacOS compatibility

--- etc/pyenv.d/install/alias-latest-patch-version.bash
+++ etc/pyenv.d/install/alias-latest-patch-version.bash
@@ -8,11 +8,18 @@
 alias_latest_patch_version__after_install_func() {
     if [ "${DEFINITION}" != "${DEFINITION_PREFIX}" ]; then
         echo "Setting alias '${DEFINITION_PREFIX}' for '${PREFIX}'"
-        # Create a relative symlink to the installed directory. The `f` ensures we overwrite any existing symlink
-        # (eg from a previous version).  The `T` is also necessary in this case, otherwise ln will silently *not*
-        # overwrite an existing directory symlink.
-        # (See https://stackoverflow.com/questions/40806543/ln-sf-does-not-overwrite-a-symlink-to-a-directory)
-        ln -sfT "${DEFINITION}" "${PREFIX}/../${DEFINITION_PREFIX}"
+        LINK_NAME="${PREFIX}/../${DEFINITION_PREFIX}"
+
+        # Delete any existing symlink (eg from a previous version).
+        # This is done separately from the ln command which though is capable of replacing an existing
+        # directory symlink via -sfT, but the T option is not available on some systems (eg MacOS).
+        #   (If you are tempted to just use `ln -sf` (without the T) be advised that this will not replace an existing
+        #   directory symlink. ln will silently *not* overwrite an existing directory symlink.
+        #   See https://stackoverflow.com/questions/40806543/ln-sf-does-not-overwrite-a-symlink-to-a-directory)
+        rm -f "${LINK_NAME}"
+
+        # Create a relative symlink to the installed directory.
+        ln -s "${DEFINITION}" "${LINK_NAME}"
     fi
 }
 
